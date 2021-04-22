3bfcaa87e94c59ccc56c5f7d107f5e83cfbfe349
aparna <aparna_patil@yahoo.com>
2021-04-22 19:40:09 +0530

    Modified 2 lines in demo3 from master

--- javaDemo3.java
+++ javaDemo3.java
@@ -2,8 +2,8 @@ class javaDemo3
 {
         public static void main(String args[])
         {
-                System.out.println("This is 3rd Java file.");
+                System.out.println("This is 3rd Java file modified in master.");
 		System.out.println("This line is added in feature1");
-		System.out.println("This line is added in feature3");
+		System.out.println("This line is added in master");
         }
 }
